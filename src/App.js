8fce914a9046bb4d4c2ec433aab3a1b6afd0f102
Kirk Thomas <dablumaroon@gmail.com>
2024-05-22 13:47:02 -0400

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react';
+import './App.css';
+import Form from './form'
+
+
+function App() {
+  return (
+    <div >
+      <div >
+        <h2>React Form Validation Demo</h2>
+      </div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
